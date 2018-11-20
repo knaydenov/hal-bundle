fd3d3e857b7e1a0c049fb7cec2d57f5fdcfdc52d
Klim Naydenov <knaydenov@gmail.com>
2018-11-21 02:21:22 +0300

    Remove managers

--- Manager/PageableManagerInterface.php
+++
@@ -1,1 +0,0 @@
-<?phpnamespace Kna\HalBundle\Manager;use Kna\HalBundle\Filter\FilterInterface;use Pagerfanta\Pagerfanta;interface PageableManagerInterface{    /**     * @param FilterInterface $filter     * @return Pagerfanta     */    public function getPager(FilterInterface $filter): Pagerfanta;}
